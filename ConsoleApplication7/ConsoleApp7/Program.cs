e4ca8f242cb80d3306a496a7b17092f216f11e00
Yusuf Yıldız <yusufyldz318@gmail.com>
2022-07-28 12:50:31 +0300

    ConsoleApplication7 eklendi

---
+++ ConsoleApplication7/ConsoleApp7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir sayi giriniz");
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 0; i < sayac; i++)
+            {
+                if(i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if(i % 2 == 1)
+                {
+                    tekToplam += i;
+                }
+                else
+                {
+                    ciftToplam += i;
+                }
+            }
+            Console.WriteLine("Tek toplam: " + tekToplam);
+            Console.WriteLine("Çift toplam: " + ciftToplam);
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                    break;
+                Console.WriteLine(i);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                    continue;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
