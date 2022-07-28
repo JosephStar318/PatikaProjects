d25ce583d0ccab4b9539b8794fea9d5240be467f
Yusuf Yıldız <yusufyldz318@gmail.com>
2022-07-28 13:17:45 +0300

    ConsoleApplication8 eklendi

---
+++ ConsoleApplication8/ConsoleApp8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayi giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while(sayac <= sayi)
+            {
+                toplam += sayi;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+            char character = 'a';
+            while(character < 'z')
+            {
+                Console.WriteLine(character);
+                character++;
+            }
+            Console.WriteLine("***********foreach");
+
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
