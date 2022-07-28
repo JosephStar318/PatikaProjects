079fb39c9da333e9d62afec4ff15851e122ba076
Yusuf Yıldız <yusufyldz318@gmail.com>
2022-07-28 12:09:26 +0300

    ConsoleApplication4 eklendi

---
+++ ConsoleApplication4/ConsoleApp4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayi " + sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("işlem tamamlandı");
+            }
+
+
+            try
+            {
+                //int a = int.Parse(null);
+                int a = int.Parse("-200000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil");
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük yada çok büyük bir değer girdiniz");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("işlem tamamlandı");
+            }
+
+        }
+    }
+}
