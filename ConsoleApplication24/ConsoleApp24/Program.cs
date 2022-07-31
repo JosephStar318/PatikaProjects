d50bc84d2fb75cd76cba56ec85bc9b68a2e7d774
Yusuf Yıldız <yusufyldz318@gmail.com>
2022-07-31 14:50:30 +0300

    ConsoleApplication24 eklendi

---
+++ ConsoleApplication24/ConsoleApp24/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+            int sicaklik = 32;
+            if(sicaklik <= (int)HavaSicakligi.Normal)
+            {
+                Console.WriteLine("Dışarıya çıkmak için biraz daha ısınmasını bekleyelim.");
+            }
+            else if(sicaklik >= (int)HavaSicakligi.Sıcak)
+            {
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak.");
+            }
+            else if(sicaklik >= (int)HavaSicakligi.Normal && sicaklik <= (int)HavaSicakligi.Sıcak)
+            {
+                Console.WriteLine("Hadi dışarı çıkalım.");
+            }
+        }
+    }
+    enum Gunler
+    {
+        Pazartesi,
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+    enum HavaSicakligi
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sıcak = 30
+    }
+}
