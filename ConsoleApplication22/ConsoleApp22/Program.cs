f0e640061478b7b2aaa283bd9f4ccb2cc8b62f7a
Yusuf Yıldız <yusufyldz318@gmail.com>
2022-07-31 14:23:27 +0300

    ConsoleApplication22 eklendi

---
+++ ConsoleApplication22/ConsoleApp22/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci = new Ogrenci();
+            ogrenci.Isim = "Ayşe";
+            ogrenci.Soyisim = "Yılmaz";
+            ogrenci.OgrenciNo = 123;
+            ogrenci.Sinif = 3;
+
+            ogrenci.SinifAtlat();
+            ogrenci.OgrenciBilgileriniGetir();
+
+            Ogrenci ogrenci1 = new Ogrenci("Deniz", "Arda", 256, 1);
+            ogrenci1.SinifDusur();
+            ogrenci1.SinifDusur();
+            ogrenci1.OgrenciBilgileriniGetir();
+
+        }
+    }
+    class Ogrenci
+    {
+        private string isim;
+        private string soyisim;
+        private int ogrenciNo;
+        private int sinif;
+
+        public string Isim { get => isim; set => isim = value; }
+        public string Soyisim { get => soyisim; set => soyisim = value; }
+        public int OgrenciNo { get => ogrenciNo; set => ogrenciNo = value; }
+        public int Sinif 
+        {
+            get => sinif;
+            set
+            {
+                if (value < 1)
+                {
+                    Console.WriteLine("Sınıf en az 1 olabilir.");
+                    sinif = 1;
+                }
+                else
+                {
+                    sinif = value;
+                }
+             
+            }
+        }
+
+        public Ogrenci(string isim, string soyisim, int ogrenciNo, int sinif)
+        {
+            this.isim = isim;
+            this.soyisim = soyisim;
+            this.ogrenciNo = ogrenciNo;
+            this.sinif = sinif;
+        }
+        public Ogrenci() { }
+        public void OgrenciBilgileriniGetir()
+        {
+            Console.WriteLine("**********Öğrenci Bilgileri***********");
+            Console.WriteLine("Öğrenci adı: {0}",this.isim);
+            Console.WriteLine("Öğrenci soyadı: {0}",this.soyisim);
+            Console.WriteLine("Öğrenci No: {0}",this.ogrenciNo);
+            Console.WriteLine("Öğrenci Sınıf: {0}",this.sinif);
+        }
+        public void SinifAtlat()
+        {
+            this.Sinif = this.Sinif + 1;
+        }
+        public void SinifDusur()
+        {
+            this.Sinif = this.Sinif - 1;
+        }
+    }
+}
