3786ed766c85357c00a8ca9d8ee3ca73850030ea
Yusuf Yıldız <yusufyldz318@gmail.com>
2022-08-05 21:32:19 +0300

    prototype 3 eklendi

---
+++ Kodla Oluştur/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float lastTimeKeyPressed = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(Time.realtimeSinceStartup - lastTimeKeyPressed > 2)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                lastTimeKeyPressed = Time.realtimeSinceStartup;
+            }
+        }
+    }
+}
